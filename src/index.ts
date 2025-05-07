a199c6a4c5f4e059e09ab34bf7a5bacdca246b59
dreznik <dreznik@rocketmail.com>
2025-05-07 14:19:58 -0400

    force build

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { RzxCommonDialogModel } from "./dialogs/RzxDialog";
 class RzxAppModel {
   @observable isBusy = false;
   @observable commonDialog = new RzxCommonDialogModel();
+  private removeLater: boolean;
 
   constructor() {
     makeObservable(this);
